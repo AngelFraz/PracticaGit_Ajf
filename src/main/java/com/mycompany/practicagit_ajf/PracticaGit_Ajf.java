caded6dd8e9458bf74657e6281446386e055bc39
valeriacarolina <valeria.chuc_pm24320@cecyte.edu.mx>
2026-03-03 07:38:55 -0600

    feat: estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/practicagit_ajf/PracticaGit_Ajf.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_ajf;
+
+/**
+ *
+ * @author Pomuch18
+ */
+public class PracticaGit_Ajf {
+
+    public static void main(String[] args) {
+     System.out.println("--- Sistema de Gestión de Alumnos ---");
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    }
+}
