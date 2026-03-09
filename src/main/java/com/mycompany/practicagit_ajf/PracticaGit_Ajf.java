c27ae83a4db71d50c57058d8f533036e96ba23a4
valeriacarolina <valeria.chuc_pm24320@cecyte.edu.mx>
2026-03-09 07:36:11 -0600

    fix: añadida calculadora básica.

--- src/main/java/com/mycompany/practicagit_ajf/PracticaGit_Ajf.java
+++ src/main/java/com/mycompany/practicagit_ajf/PracticaGit_Ajf.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.practicagit_ajf;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch18
@@ -13,7 +15,23 @@ public class PracticaGit_Ajf {
     public static void main(String[] args) {
      System.out.println("--- Sistema de Gestión de Alumnos ---");
      System.out.println("Bienvenido, Usuario Estudiante.");
-     
+      Scanner  cin = new Scanner(System.in);
+        
+        
+        int n1, n2, suma;
+        
+        
+        System.out.print("Introduce primer numero: ");
+        n1 = cin.nextInt();
+        
+        System.out.print("Introduce segundo numero ");
+        n2 = cin.nextInt();
+        
+        
+        suma = n1 + n2;
+        
+        
+        System.out.println("El resultado es: " + suma);
      
      
      
